33b315c73948e97b62f22eb5749679397d13a812
ym1085 <youngmin1085@gmail.com>
2021-11-20 16:28:14 +0900

    Add image upload
    1. 이미지 업로드 기능 구현
    2. urls.py, 메인 화면 이미지 안 나오는 부분 수정

--- photo/urls.py
+++ photo/urls.py
@@ -6,8 +6,14 @@ from .views import PhotoList, PhotoCreate, PhotoDelete, PhotoDetail, PhotoUpdate
 app_name = "photo"
 urlpatterns = [
     path("", PhotoList.as_view(), name='index'),
-    path("create/", PhotoCreate.as_view, name='create'),
-    path("delete/<int:pk>", PhotoDelete.as_view, name='delete'),
-    path("update/<int:pk>", PhotoUpdate.as_view, name='update'),
-    path("detail/<int:pk>", PhotoDetail.as_view, name='detail'),
+    path("create/", PhotoCreate.as_view(), name='create'),
+    path("delete/<int:pk>", PhotoDelete.as_view(), name='delete'),
+    path("update/<int:pk>", PhotoUpdate.as_view(), name='update'),
+    path("detail/<int:pk>", PhotoDetail.as_view(), name='detail'),
 ]
+
+# 메인 화면 이미지 안 나오는 부분 패키지 추가
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- photo/views.py
+++ photo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.detail import DetailView
@@ -12,14 +12,22 @@ class PhotoList(ListView):
 # Create
 class PhotoCreate(CreateView):
     model = Photo
-    fields = ['text', 'image']
+    fields = ['author', 'text', 'image']
     template_name_suffix = '_create'
     success_url = '/'
+
+    def from_valid(self, form):
+        form.instance.author_id = self.request.user.id
+        if form.is_valid():
+            form.instance.save()
+            return redirect('/')
+        else:
+            return self.render_to_response({'form':form})
 
 # Update
 class PhotoUpdate(UpdateView):
     model = Photo
-    fields = ['text', 'image']
+    fields = ['author', 'text', 'image']
     template_name_suffix = '_update'
     success_url = '/'
 
