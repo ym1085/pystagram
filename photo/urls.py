721b70c2a524f9a0eaf8f5f368bdb7367aa87d29
ym1085 <youngmin1085@gmail.com>
2021-11-20 15:26:03 +0900

    Add instagram main list
    1. 인스타그램 메인 리스트 화면 완료

--- photo/urls.py
+++ photo/urls.py
@@ -5,7 +5,7 @@ from .views import PhotoList, PhotoCreate, PhotoDelete, PhotoDetail, PhotoUpdate
 
 app_name = "photo"
 urlpatterns = [
-    path("", PhotoList.as_view(), name='index'), # 아무것도 안 들어온 경우
+    path("", PhotoList.as_view(), name='index'),
     path("create/", PhotoCreate.as_view, name='create'),
     path("delete/<int:pk>", PhotoDelete.as_view, name='delete'),
     path("update/<int:pk>", PhotoUpdate.as_view, name='update'),
