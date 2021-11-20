a1b14bfedcd197bc053027e78ae37fa90dc4980c
ym1085 <youngmin1085@gmail.com>
2021-11-20 17:56:24 +0900

    Update accounts app
    1. 수정/삭제 권한에 따라 메시지 출력
    2. dispatch 메서드 추가

--- photo/views.py
+++ photo/views.py
@@ -1,15 +1,15 @@
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.detail import DetailView
 from photo.models import Photo
 
-# List
 class PhotoList(ListView):
     model = Photo
     template_name_suffix = '_list'
 
-# Create
 class PhotoCreate(CreateView):
     model = Photo
     fields = ['text', 'image']
@@ -25,20 +25,37 @@ class PhotoCreate(CreateView):
         else:
             return self.render_to_response({'form':form})
 
-# Update
 class PhotoUpdate(UpdateView):
     model = Photo
     fields = ['text', 'image']
     template_name_suffix = '_update'
     success_url = '/'
 
-# Delete
+    def dispatch(self, request, *args, **kwargs):
+        print("author = {0}".format(self.get_object().author))
+        print("request = {0}".format(request.user))
+        object = self.get_object()
+        if object.author != request.user:
+            messages.warning(request, "수정할 권한이 없습니다.")
+            return HttpResponseRedirect('/')
+        else:
+            return super(PhotoUpdate, self).dispatch(request, *args, **kwargs) # PhotoUpdate를 다시 시작
+
 class PhotoDelete(DeleteView):
     model = Photo
     template_name_suffix = '_delete'
     success_url = '/'
 
-# Detail
+    def dispatch(self, request, *args, **kwargs):
+        print("author = {0}".format(self.get_object().author))
+        print("request = {0}".format(request.user))
+        object = self.get_object()
+        if object.author != request.user:
+            messages.warning(request, "삭제할 권한이 없습니다.")
+            return HttpResponseRedirect('/')
+        else:
+            return super(PhotoDelete, self).dispatch(request, *args, **kwargs) # PhotoDelete를 다시 시작
+
 class PhotoDetail(DetailView):
     model = Photo
     template_name_suffix = '_detail'
